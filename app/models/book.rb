7a3d71a6f0e77ba3d290f86da85c696f1d2de939
Stefan Bunk <stefan.bunk@googlemail.com>
2012-09-16 14:57:01 +0200

    Add validation for books.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ActiveRecord::Base
   attr_accessible :author, :title
+
+  validates :author, :presence => true
+  validates :title,  :presence => true,
+                     :length => { :minimum => 5 }
 end
