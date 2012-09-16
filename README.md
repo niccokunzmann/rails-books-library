8dad38ebdea8b618b5bcb78177855319e1e86d8f
Stefan Bunk <stefan.bunk@googlemail.com>
2012-09-16 12:41:21 +0200

    Run 'rails new rails-books-library'.

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-rails-books-library
-===================
+# rails-books-library
 
 Small book management tool written to get familiar with rails #BP2012H1
 
@@ -8,3 +7,6 @@ Useful links:
 * [Rails Migrations](http://guides.rubyonrails.org/migrations.html)
 
 I think we should use this readme or the wiki to gather our insights.
+
+## Steps so far
+* `rails new rails-books-library` generated the skeleton of the rails app with all necessary folders
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBooksLibrary::Application.config.secret_token = '79364f2b688749706cf1adf9526f01346736c9518cca4e5f180c17776bfbb9d5e514a326976a2fd7f28939bbf6b64916a9e157bd5319e02b2b629954fdf6bd1b'
