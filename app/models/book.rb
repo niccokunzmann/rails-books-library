ae63f636eeedb0b6f8b37789b7e7fa43f2535f9b
Dimitri Korsch <korschdima@yahoo.de>
2012-10-16 10:48:37 +0200

    constrain: buch kann 0 oder 1 ausleihen angehören

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,9 @@ class Book < ActiveRecord::Base
   validates :author, :presence => true
   validates :title,  :presence => true,
                      :length => { :minimum => 5 }
+  validates :lending, 
 
   has_many :comments, :dependent => :destroy
+
+  has_one :lending, :allow_nil => true
 end
