cf4d1ffcd4091018f721c0b7e955b32f97e48270
Matthias Springer <me@matthiasspringer.de>
2012-10-19 16:18:17 +0200

    Changed hash syntax for books.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,8 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @books }
-	  format.xml { render xml: @books }
+      format.json { render :json => @books }
+	  format.xml { render :xml => @books }
     end
   end
 
@@ -22,7 +22,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @book }
+      format.json { render :json => @book }
     end
   end
 
@@ -33,7 +33,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @book }
+      format.json { render :json => @book }
     end
   end
 
@@ -49,11 +49,11 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to @book, notice: 'Book was successfully created.' }
-        format.json { render json: @book, status: :created, location: @book }
+        format.html { redirect_to @book, :notice => 'Book was successfully created.' }
+        format.json { render :json => @book, :status => :created, :location => @book }
       else
-        format.html { render action: "new" }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
+        format.html { render :action => "new" }
+        format.json { render :json => @book.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -65,11 +65,11 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       if @book.update_attributes(params[:book])
-        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
+        format.html { redirect_to @book, :notice => 'Book was successfully updated.' }
         format.json { head :no_content }
       else
-        format.html { render action: "edit" }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
+        format.html { render :action => "edit" }
+        format.json { render :json => @book.errors, :status => :unprocessable_entity }
       end
     end
   end
