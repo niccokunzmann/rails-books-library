9f1973ebecc6dc686e264973e9b2762ad642ab5e
Dimitri Korsch <korschdima@yahoo.de>
2012-12-12 10:40:52 +0100

    created attr_accessor and validates

--- app/models/lending.rb
+++ app/models/lending.rb
@@ -1,7 +1,10 @@
-class Lending < ActiveRecord::Base
+class Lending
+  include Maglev::Base
+  
   attr_accessible :lendingDate, :user, :book, :book_id, :user_id
-  belongs_to :user
-  belongs_to :book
+  
+  # belongs_to :user
+  # belongs_to :book
 
   validates :user, :presence => true
   validates :book, :presence => true
