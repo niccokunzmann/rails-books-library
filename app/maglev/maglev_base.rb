35d9c364026f11d8152133f612c70055c08a5d68
Dimitri Korsch <korschdima@yahoo.de>
2012-12-12 10:44:31 +0100

    Revert "created attr_accessor and validates"
    This reverts commit 9f1973ebecc6dc686e264973e9b2762ad642ab5e.

--- app/maglev/maglev_base.rb
+++ app/maglev/maglev_base.rb
@@ -1,45 +1,28 @@
 module Maglev
   module Base
-     #include ActiveModel::MassAssignmentSecurity
-    # include ActiveModel::Validations
+    include ActiveModel::MassAssignmentSecurity
+    include ActiveModel::Validations
     # include ActiveRecord::Associations
     # include ActiveRecord::Reflection
 
-    # puts "USEFUL INFORMATIONS"
-    # puts Maglev::PERSISTENT_ROOT
-    # puts self
-    # Maglev::PERSISTENT_ROOT[self] ||= Hash.new
-    # puts Maglev::PERSISTENT_ROOT
-    module ClassMethods
-      
-      def find(id)
-        Maglev::PERSISTENT_ROOT[self][id]
-      end
+    puts "USEFUL INFORMATIONS"
+    puts Maglev::PERSISTENT_ROOT
+    puts self
+    Maglev::PERSISTENT_ROOT[self] ||= Hash.new
+    puts Maglev::PERSISTENT_ROOT
 
-      def all
-        Maglev::PERSISTENT_ROOT[self].values
-      end
-
-      def create(*args)
-        x = new(*args)
-        x
-      end
-
-      # def attr_accessible(*args)
-      # end
-
-      def validates(*args)
-      end
-
+    def self.find(id)
+      Maglev::PERSISTENT_ROOT[self][id]
     end
 
-    def self.included(host)
-      # TODO: Almost certainly not a good thing beyond the dev env: This will
-      # blow away any persistent data from previous runs.  Perhaps we need a
-      # migration idiom so that this can be controlled.
-      Maglev::PERSISTENT_ROOT[host] = Hash.new
-      host.extend ClassMethods
-      host.extend Enumerable
+    def self.all
+      Maglev::PERSISTENT_ROOT[self].values
     end
+
+    def self.create(*args)
+      x = self.new(*args)
+      x
+    end
+
   end
 end
--- app/models/lending.rb
+++ app/models/lending.rb
@@ -1,10 +1,7 @@
-class Lending
-  include Maglev::Base
-  
+class Lending < ActiveRecord::Base
   attr_accessible :lendingDate, :user, :book, :book_id, :user_id
-  
-  # belongs_to :user
-  # belongs_to :book
+  belongs_to :user
+  belongs_to :book
 
   validates :user, :presence => true
   validates :book, :presence => true
